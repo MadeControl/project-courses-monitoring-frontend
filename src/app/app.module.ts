acf1b17a82d7ed1d1e64c5e23632fb1a6c60049d
MadeControl <danya.polishchuk.2001@ukr.net>
2021-04-14 18:09:18 +0300

    add - header, main content, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './main/ui/footer/footer.component';
+import { MainContentComponent } from './main/ui/main-content/main-content.component';
+import { HeaderComponent } from './main/ui/header/header.component';
+import { CoursesListComponent } from './main/ui/main-content/courses-list/courses-list.component';
+import { CourseCardComponent } from './main/ui/main-content/courses-list/course-card/course-card.component';
+import { CourseInfoComponent } from './main/ui/main-content/course-info/course-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainContentComponent,
+    HeaderComponent,
+    CoursesListComponent,
+    CourseCardComponent,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
